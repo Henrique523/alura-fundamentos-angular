2d8e10b16e7356e3ba685629d8620d66326673dd
Guilherme Henrique Lemes de Godoy <guilherme.hlg.eng@gmail.com>
2022-11-18 11:22:10 -0300

    finaliza capitulo de autenticacao e o papel do token

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {tap} from "rxjs";
+import {TokenService} from "../token/token.service";
 
 const API_URL = 'http://localhost:3000'
 
@@ -8,9 +10,21 @@ const API_URL = 'http://localhost:3000'
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private tokenService: TokenService
+  ) {}
 
   authenticate(userName: string, password: string) {
-    return this.httpClient.post(API_URL + '/user/login', { userName, password })
+    return this.httpClient.post(
+      API_URL + '/user/login',
+      { userName, password },
+      { observe: 'response'}
+    )
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token')!;
+        this.tokenService.setToken(authToken);
+        console.log(`User ${userName} authenticated with token ${authToken}`);
+      }))
   }
 }
